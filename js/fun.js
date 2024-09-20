3103b9c2df7192f1ce3a4c8df763b4e448d8981c
erick.pita <erick.pita@sasf.net>
2024-09-20 14:40:27 -0500

    v1.0.2

--- js/fun.js
+++ js/fun.js
@@ -1,5 +1,15 @@
-(()=>{
-    const select = document.getElementById("categoria");
-    select.innerHTML="<option>Valor</option>";
-    console.log("OKOKOK")
+const listCategorias = [
+  "Del Hogar",
+  "Ferreteria",
+  "Construccion",
+  "Decoracion y Acabados",
+];
+
+(() => {
+  const select = document.getElementById("categoria");
+  select.innerHTML = "<option>Seleccione..</option>";
+  listCategorias.forEach((x) => {
+    console.log(x);
+    select.innerHTML += "<option>" + x + "</option>";
+  });
 })();
